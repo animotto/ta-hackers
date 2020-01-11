2f4ddfaefb63750a0ba135ce684bea6f48457aba
anim <anton@telpart.ru>
2020-01-11 18:58:54 +0300

    Check connection script improving

--- scripts/checkcon.rb
+++ scripts/checkcon.rb
@@ -3,16 +3,11 @@ class Checkcon < Sandbox::Script
   
   def main
     loop do
+      @game.cmdCheckCon
+    rescue Trickster::Hackers::RequestError => e
+      @shell.log("#{e.type}: #{e.description}", :script)
+    ensure
       sleep(INTERVAL)
-
-      msg = "Check connectivity"
-      begin
-        @game.cmdCheckCon
-      rescue Trickster::Hackers::RequestError => e
-        @shell.log("#{e.type}: #{e.description}", :script)
-        return
-      end
-      @shell.log(msg, :success)
     end
   end
 end
