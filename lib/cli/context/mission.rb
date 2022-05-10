dffb7509e490c0c70a6e9b76bf9ef7af264c3971
anim <anton@telpart.ru>
2022-05-10 17:52:19 +0300

    Refactoring mission context

---
+++ lib/cli/context/mission.rb
@@ -0,0 +1,104 @@
+# frozen_string_literal: true
+
+## Commands
+
+# list
+CONTEXT_MISSION.add_command(
+  :list,
+  description: 'Show missions log'
+) do |shell, context, tokens|
+  if GAME.sid.empty?
+    shell.puts('No session ID')
+    next
+  end
+
+  msg = 'Missions get log'
+  missions = GAME.cmdPlayerMissionsGetLog
+  LOGGER.log(msg)
+
+  shell.puts("\e[1;35m\u2022 Missions log\e[0m")
+  shell.puts(
+    format(
+      "  \e[35m%-1s %-7s %-7s %-8s %-20s\e[0m",
+      '',
+      'ID',
+      'Money',
+      'Bitcoins',
+      'Datetime'
+    )
+  )
+
+  missions.each do |k, v|
+    status = String.new
+    case v['finished']
+    when Trickster::Hackers::Game::MISSION_AWAITS
+      status = "\e[37m\u2690\e[0m" 
+    when Trickster::Hackers::Game::MISSION_FINISHED
+      status = "\e[32m\u2691\e[0m"
+    when Trickster::Hackers::Game::MISSION_REJECTED
+      status = "\e[31m\u2691\e[0m"
+    end
+
+    shell.puts(
+      format(
+        '  %-1s %-7d %-7d %-8d %-20s',
+        status,
+        k,
+        v['money'],
+        v['bitcoins'],
+        v['datetime']
+      )
+    )
+  end
+rescue Trickster::Hackers::RequestError => e
+  LOGGER.error("#{msg} (#{e})")
+end
+
+# start
+CONTEXT_MISSION.add_command(
+  :start,
+  description: 'Start mission',
+  params: ['<id>']
+) do |shell, context, tokens|
+  if GAME.sid.empty?
+    shell.puts('No session ID')
+    next
+  end
+
+  id = tokens[1].to_i
+
+  unless GAME.missionsList.key?(id)
+    shell.puts('No such mission')
+    next
+  end
+
+  msg = 'Mission message delivered'
+  GAME.cmdPlayerMissionMessageDelivered(id)
+  LOGGER.log(msg)
+rescue Trickster::Hackers::RequestError => e
+  LOGGER.error("#{msg} (#{e})")
+end
+
+# reject
+CONTEXT_MISSION.add_command(
+  :reject,
+  description: 'Reject mission',
+  params: ['<id>']
+) do |shell, context, tokens|
+  if GAME.sid.empty?
+    shell.puts('No session ID')
+    next
+  end
+
+  id = tokens[1].to_i
+  unless GAME.missionsList.key?(id)
+    shell.puts('No such mission')
+    next
+  end
+
+  msg = 'Mission reject'
+  GAME.cmdPlayerMissionReject(id)
+  LOGGER.log(msg)
+rescue Trickster::Hackers::RequestError => e
+  LOGGER.error("#{msg} (#{e})")
+end
