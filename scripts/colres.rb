754ef0400a7e800454693ee89b0f6fdcf43935c4
anim <anton@telpart.ru>
2022-05-19 03:39:53 +0300

    Resource collection script improving

--- scripts/colres.rb
+++ scripts/colres.rb
@@ -1,6 +1,7 @@
 class Colres < Sandbox::Script
   INTERVAL_MIN = 300
   INTERVAL_ADD = 120
+  PERCENT = 90
 
   def main
     unless @game.connected?
@@ -10,13 +11,24 @@ class Colres < Sandbox::Script
 
     loop do
       @game.player.load
-
       @game.player.net.each do |node|
         next unless node.kind_of?(Hackers::Nodes::Production)
 
         node_type = @game.node_types.get(node.type)
+        total_time = node_type.production_limit(node.level).to_f / node_type.production_speed(node.level) * 60 * 60
+        percent = node.timer / total_time * 100
+        next if percent < PERCENT
+
         node.collect
-        @logger.log("Node #{node_type.name} (#{node.id}) resources collected")
+
+        collected = ((node_type.production_speed(node.level).to_f / 60 / 60) * node.timer).to_i
+        case node_type.production_currency(node.level)
+        when Hackers::Network::CURRENCY_MONEY
+          currency = '$'
+        when Hackers::Network::CURRENCY_BITCOINS
+          currency = "\u20bf"
+        end
+        @logger.log("Node #{node_type.name} (#{node.id}) collected #{collected}#{currency}")
       end
     rescue Hackers::RequestError => e
       @logger.error(e)
